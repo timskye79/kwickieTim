7c1ee8a9cfb666bf89f22d49329c793d54b5e531
Timothy Bowers <timothy.skye.bowers.79@gmail.com>
2016-03-13 09:53:09 +1000

    Initial Commit

---
+++ kwickieTim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kwickieTim
+//
+//  Created by Test on 13/03/2016.
+//  Copyright © 2016 Tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kwickieTim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kwickieTim
+//
+//  Created by Test on 13/03/2016.
+//  Copyright © 2016 Tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
